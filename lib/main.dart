fcf061396b70caa3b4d934b7f35fa4da491f1a89
Mr. Fahim <fahimzihad@gmail.com>
2023-12-16 19:27:33 +0600

    Cloud Message with Token and Subcribe features

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_learning/firebase_messaging_service.dart';
 import 'package:firebase_learning/firebase_options.dart';
 import 'package:firebase_learning/student_details.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
+
+
+Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+await FirebaseMessagingService().initialise();
+await FirebaseMessagingService().getFCMToken();
+await FirebaseMessagingService().subscribeToTopic('The-New-Saudi');
+await FirebaseMessagingService().onRefresh((token) {
+  //TODO - send to API
+});
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,3 +29,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
