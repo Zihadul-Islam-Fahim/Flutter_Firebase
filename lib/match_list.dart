7edf17fc60ce41d36f6552b01eac9aec553b7fc1
Mr. Fahim <fahimzihad@gmail.com>
2023-12-17 12:40:48 +0600

    get instant Change by using StreamBuilder

--- lib/match_list.dart
+++ lib/match_list.dart
@@ -13,27 +13,28 @@ class MatchList extends StatefulWidget {
 class _MatchListState extends State<MatchList> {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   List<Match> matchList = [];
+  late final table = _firebaseFirestore.collection("matchScore");
 
-  Future<void> getmatchList() async {
-    matchList.clear();
-    final table = _firebaseFirestore.collection("matchScore");
-    final QuerySnapshot data = await table.get();
-    for (QueryDocumentSnapshot element in data.docs) {
-      Match match = Match(
-          matchName: element["Teams"],
-          score: element["Score"],
-          time: element["Time"],
-          totalTime: element["TotalTime"]);
-      matchList.add(match);
-      setState(() {});
-    }
-  }
-
-  @override
-  void initState() {
-    getmatchList();
-    super.initState();
-  }
+  // Future<void> getmatchList() async {
+  //   matchList.clear();
+  //
+  //   final QuerySnapshot data = await table.get();
+  //   for (QueryDocumentSnapshot element in data.docs) {
+  //     Match match = Match(
+  //         matchName: element["Teams"],
+  //         score: element["Score"],
+  //         time: element["Time"],
+  //         totalTime: element["TotalTime"]);
+  //     matchList.add(match);
+  //     setState(() {});
+  //   }
+  // }
+  //
+  // @override
+  // void initState() {
+  //   // getmatchList();
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -41,29 +42,50 @@ class _MatchListState extends State<MatchList> {
       appBar: AppBar(
         title: const Text('Match List'),
       ),
-      body: RefreshIndicator(
-        onRefresh: getmatchList,
-        child: ListView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: matchList.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                trailing: const Icon(Icons.arrow_forward),
-                title: Text(matchList[index].matchName,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MatchDetailspage(matchList[index]),
+      body: StreamBuilder(
+          stream: table.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              matchList.clear();
+              for (QueryDocumentSnapshot element in snapshot.data!.docs) {
+                Match match = Match(
+                    matchName: element["Teams"],
+                    score: element["Score"],
+                    time: element["Time"],
+                    totalTime: element["TotalTime"]);
+                matchList.add(match);
+              }
+              return ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemCount: matchList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      trailing: const Icon(Icons.arrow_forward),
+                      title: Text(
+                        matchList[index].matchName,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 18),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                MatchDetailspage(matchList[index]),
+                          ),
+                        );
+                      },
                     ),
                   );
                 },
-              ),
-            );
-          },
-        ),
-      ),
+              );
+            }
+            else{
+              return const Center(child: LinearProgressIndicator());
+            }
+          }
+          ),
     );
   }
 }
